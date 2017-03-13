df12ed799bae31c6040ab436e09eebdbc205ab3a
Jonathan Ku <jku@mediamath.com>
2017-03-12 21:16:56 -0400

    Initial functions

---
+++ test/name.test.js
@@ -0,0 +1,36 @@
+
+// import {expect} from 'chai';
+
+// import { checkPosition, outDirectionString, testIt } from '../lib/name';
+var expect = require('chai').expect;  
+// var Camo = require('camo');  
+var testMe = require("../lib/name.js");
+
+var testArray = ['5x5','(1,2)'];
+var wrongArray = ['5x', '(1,2)'];
+
+var gridCorrect = ['5x5','(1,2)'];
+var gridWrong = ['5x5','(6,6)'];
+// console.log(testMe);
+
+describe('gridCorrect', function() {
+  it('should work!', function() {
+    expect(testMe.gridCorrect(testArray)).to.be.true;
+  });
+
+  it('should not work!', function() {
+    expect(testMe.gridCorrect(wrongArray)).to.be.false;
+  });
+});
+
+describe('pointsWithinGrid', function() {
+  it('should work!', function() {
+    expect(testMe.pointsWithinGrid(gridCorrect)).to.be.true;
+  });
+
+  it('should not work!', function() {
+    expect(testMe.pointsWithinGrid(gridWrong)).to.be.false;
+  });
+});
+
+
